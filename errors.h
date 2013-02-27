515a1656f992ca34a773b80dff909ced7a7d74ec
Dron Rathore <dron.rathore@gmail.com>
2013-02-27 10:07:18 +0530

    Create errors.h

---
+++ errors.h
@@ -0,0 +1,6 @@
+#define WSOCK_VER_NOT_FOUND "WSOCK_VER_NOT_FOUND:Winsock Version 2.2 Required"
+#define INVALID_PORT_TO_BIND "INVALID_PORT_TO_BIND:Port number should be greater than 10"
+#define BIND_FAILURE "BIND_FAILURE: Cannot Bind to the defined port."
+#define LISTEN_FAILURE "LISTEN_FAILURE: Cannot Listen on the defined port, port in use or permision denied."
+#define SOCKET_CREATION_FAILED "SOCKET_CREATION_FAILED: Could'nt Create Socket!"
+typedef const char* Text;
